9a302c8e8a085ec92fc1486568269bcd8c30069d
boyitcoder2004 <thienitcoder2004@gmail.com>
2025-10-06 14:27:36 +0700

    giao diện trang home

--- website-e-commerce/obj/Debug/net9.0/website-e-commerce.AssemblyInfo.cs
+++ website-e-commerce/obj/Debug/net9.0/website-e-commerce.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("website-e-commerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05e5b80a11d52809d77fb024c9dd517bd84dd894")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fc5c28edef1f7002c7749705722e10b65bbb2c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("website-e-commerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("website-e-commerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
