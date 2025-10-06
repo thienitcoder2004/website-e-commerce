339910b5d7c0d24279e175c5faef7041ef09dec7
boyitcoder2004 <thienitcoder2004@gmail.com>
2025-10-06 20:43:12 +0700

    Fix xung đột

--- website-e-commerce/obj/Debug/net9.0/website-e-commerce.AssemblyInfo.cs
+++ website-e-commerce/obj/Debug/net9.0/website-e-commerce.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("website-e-commerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9716430367a932db04b44eeb0751676c2b585046")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5839584703e6d6e8e47a1679bc7ce7b09dbcd07b")]
 [assembly: System.Reflection.AssemblyProductAttribute("website-e-commerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("website-e-commerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
